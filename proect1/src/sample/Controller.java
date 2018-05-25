43931ff0863d97dddb09e47e0cb495223bd3c2ef
Aleksand012 <lipin012@yandex.ru>
2018-05-25 15:56:35 +1000

    b

--- proect1/src/sample/Controller.java
+++ proect1/src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.scene.control.*;
+
+import java.awt.*;
+
 public class Controller {
+
+    public javafx.scene.control.TextField Wvod;
+    
+
+    public void nDs() {
+
+    }
 }
