e7c9ed2f52b34334c47e09fb5e7c1eb831ee9b76
Aleksand012 <lipin012@yandex.ru>
2018-05-25 16:59:33 +1000

    VBox

--- proect1/src/sample/Controller.java
+++ proect1/src/sample/Controller.java
@@ -1,13 +1,16 @@
 package sample;
 
 import javafx.scene.control.*;
+import javafx.scene.control.TextArea;
 
 import java.awt.*;
 
 public class Controller {
 
-    public javafx.scene.control.TextField Wvod;
-    
+    public javafx.scene.control.TextField vod;
+    public TextArea bNds;
+
+
 
     public void nDs() {
 
